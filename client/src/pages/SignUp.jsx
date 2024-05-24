974222661e74a895c8cea90623f6a30446cc3104
nminh <minhlui254qn254@gmail.com>
2024-05-24 23:20:35 +0700

    redux persist

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -47,7 +47,7 @@ export default function SignUp() {
         <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email' onChange={handleChange} />
         <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' onChange={handleChange} />
         <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-          {loading ? 'Loading...' : 'Sign up'}
+          {loading ? 'Loading...' : 'Sign Up'}
         </button>
       </form>
       <div className='flex gap-2 mt-5'>
