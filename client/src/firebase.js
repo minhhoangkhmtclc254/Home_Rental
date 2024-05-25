ef8839301c565dc57d0b878964434160cad4ac5c
nminh <minhlui254qn254@gmail.com>
2024-05-25 22:07:32 +0700

    image upload

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "home-mern.firebaseapp.com",
+  projectId: "home-mern",
+  storageBucket: "home-mern.appspot.com",
+  messagingSenderId: "467934822606",
+  appId: "1:467934822606:web:e4003970640424ddd8109f"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
